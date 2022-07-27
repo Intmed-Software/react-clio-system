d269e49135142eb14a3a03b01096d2a59ecc8831
bismarck <bismarckalvess@gmail.com>
2022-07-27 01:11:15 -0300

    Add Avatar to clio-system

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@ import colors from "./src/patters/colors";
 import sizes from "./src/patters/sizes";
 import weights from "./src/patters/weights";
 
+export { Avatar } from "./src/components/Avatar";
+export { AvatarProps } from "./src/components/Avatar/types";
 export * from "./src/components/Button";
 export * from "./src/components/Chips";
 export * from "./src/components/CircularProgress";
@@ -9,11 +11,10 @@ export * from "./src/components/Dialog";
 export * from "./src/components/Drawer";
 export * from "./src/components/Icons";
 export * from "./src/components/Label";
+export * from "./src/components/Radio";
 export * from "./src/components/TextArea";
 export * from "./src/components/TextField";
 export * from "./src/components/Toolbar";
 export * from "./src/components/Typography";
-export * from "./src/components/Radio";
-
 export { colors, sizes, weights };
 
---
+++ src/stories/components/Avatar.stories.tsx
@@ -0,0 +1,90 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+import { Avatar } from "../../components/Avatar";
+import { apiDescription } from "./utils/ApiDescription";
+
+export default {
+  title: "components/Avatar",
+  component: Avatar,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: apiDescription(["Avatar"]),
+      },
+    },
+  },
+} as ComponentMeta<typeof Avatar>;
+
+const Template: ComponentStory<typeof Avatar> = (args) => <Avatar {...args} />;
+
+const img =
+  "https://arcosmodels.com/wp-content/uploads/2021/05/capa-aline-de-assis-290x380.jpg";
+const base = {
+  label: "José Maria",
+};
+export const InitialsSmall = Template.bind({});
+
+InitialsSmall.args = {
+  ...base,
+  ...{
+    size: "small",
+    background: "mediumBlue",
+  },
+};
+
+export const InitialsMedium = Template.bind({});
+
+InitialsMedium.args = {
+  ...base,
+  ...{
+    size: "medium",
+    background: "darkBlue",
+  },
+};
+
+export const InitialsLarge = Template.bind({});
+
+InitialsLarge.args = {
+  ...base,
+  ...{
+    size: "large",
+    background: "mainOrange",
+  },
+};
+
+export const PhotoSmall = Template.bind({});
+
+PhotoSmall.args = {
+  ...base,
+  ...{
+    size: "small",
+    background: "mainOrange",
+    img,
+  },
+};
+
+export const PhotoMedium = Template.bind({});
+
+PhotoMedium.args = {
+  ...base,
+  ...{
+    size: "medium",
+    background: "mainOrange",
+    img,
+  },
+};
+
+
+export const PhotoLarge = Template.bind({});
+
+PhotoLarge.args = {
+  ...base,
+  ...{
+    size: "large",
+    background: "mainOrange",
+    img,
+  },
+};
